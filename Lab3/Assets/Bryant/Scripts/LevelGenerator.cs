9a433e3b8c10aeaee998b98e2b3265ca4b0f0197
iAmJiro <115590354+iAmJiro@users.noreply.github.com>
2024-10-29 14:45:16 +1300

    start for lab 3

---
+++ Lab3/Assets/Bryant/Scripts/LevelGenerator.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public int levelWidth, levelHeight, roomSize;
+    public Material gridMat1, gridMat2;
+    GameObject levelGrid;
+
+    [Header("Tracking Rooms")]
+    public Room currentRoom;
+    public GameObject player;
+
+    [Header("From Rooms")]
+    public GameObject roomPrefab, levelRooms;
+    public Room[,] rooms;
+
+
+    void Start()
+    {
+        GenerateGrid();
+        CreateRooms();
+        CreatePath();
+    }
+    void GenerateGrid()
+    {
+        levelGrid = new GameObject("_LevelGrid");
+        levelGrid.transform.position = new Vector3((-levelWidth / 2) * roomSize, 0, (levelHeight / 2) * roomSize);
+        Material activeGridMat = gridMat1;
+        for (int y = 0; y < levelWidth; y++)
+        {
+            for (int x = 0; x < levelHeight; x++)
+            {
+                GameObject floorTile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                floorTile.transform.parent = levelGrid.transform;
+                floorTile.transform.localScale = new Vector3(roomSize, roomSize, 1);
+                floorTile.transform.localEulerAngles = new Vector3(90, 0, 0);
+                floorTile.transform.localPosition = new Vector3(x * roomSize, 0, -y * roomSize);
+                //alternate
+                floorTile.GetComponent<MeshRenderer>().material = activeGridMat;
+                if (activeGridMat == gridMat1)
+                {
+                    activeGridMat = gridMat2;
+                }
+                else
+                {
+                    activeGridMat = gridMat1;
+                }
+            }
+        }
+    }
+    void CreateRooms()
+    {
+        levelRooms = new GameObject("_LevelRooms");
+        levelRooms.transform.position = new Vector3((-levelWidth / 2) *
+        roomSize, 0, (levelHeight / 2) * roomSize);
+        rooms = new Room[levelWidth, levelHeight];
+        for (int y = 0; y < levelWidth; y++)
+        {
+            for (int x = 0; x < levelHeight; x++)
+            {
+                GameObject room = (GameObject)Instantiate(roomPrefab, levelRooms.transform);
+                room.name = "Room : " + x + "," + y;
+                room.transform.localPosition = new Vector3(x * roomSize, 0, -y * roomSize);
+                Room roomInfo = room.GetComponent<Room>();
+                roomInfo.gridX = x;
+                roomInfo.gridY = y;
+                roomInfo.UpdateInfo();
+                rooms[x, y] = roomInfo;
+            }
+        }
+    }
+    void CreatePath() 
+    {
+        int entranceX = Random.Range(0, levelWidth - 1);
+        currentRoom = rooms[entranceX, 0];
+        Room roomInfo = rooms[entranceX, 0].GetComponent<Room>();
+        roomInfo.onPath = true;
+        roomInfo.isEntrance = true;
+        roomInfo.UpdateInfo();
+        //put player in entrance
+        player.transform.position = roomInfo.transform.position;
+        FindNextRoom();
+    }
+    public void FindNextRoom() 
+    {
+        currentRoom.neighbours = GetNeighbours(currentRoom);
+        //make sure that there are neighbours
+        if (currentRoom.neighbours.Count > 0) 
+        {
+            //select random room that is neighbours
+            int nextIndex = Random.Range(0, currentRoom.neighbours.Count);
+            Room nextRoom = currentRoom.neighbours[nextIndex];
+            nextRoom.onPath = true;
+            //deternmine room openings
+            if(nextRoom.gridX == currentRoom.gridX - 1) 
+            {
+                //Right -> Left
+                currentRoom.openings.Add("L");
+                nextRoom.openings.Add("R");
+            }
+            if (nextRoom.gridX == currentRoom.gridX + 1) 
+            {
+                //Left -> Right
+                currentRoom.openings.Add("R");
+                nextRoom.openings.Add("L");
+            }
+            if (nextRoom.gridY == currentRoom.gridY + 1)
+            {
+                //Up -> Down
+                currentRoom.openings.Add("D");
+                nextRoom.openings.Add("U");
+            }
+            nextRoom.UpdateInfo();
+            currentRoom = nextRoom;
+            FindNextRoom();
+        }
+        else
+        {
+            currentRoom.isExit = true;
+            currentRoom.UpdateInfo();
+            PlaceRooms();
+        }
+    }
+    public List<Room> GetNeighbours(Room room) 
+    {
+        List<Room> neighbours = new List<Room>();
+        //check left neighbour
+        if (room.gridX - 1 >= 0) 
+        {
+            Room leftRoom = rooms[room.gridX = 1, room.gridY];
+            if (!leftRoom.onPath)
+            {
+                neighbours.Add(leftRoom);
+            }
+        }
+        //check right neighbour
+        if (room.gridX + 1 < levelWidth) 
+        {
+            Room rightRoom = rooms[room.gridX = 1, room.gridY];
+            if (!rightRoom.onPath) 
+            {
+                neighbours.Add(rightRoom);
+            }
+        }
+        //check bottom Neighbour
+        if (room.gridY < levelHeight - 1) 
+        {
+            Room downRoom = rooms[room.gridX, room.gridY + 1];
+            if (!downRoom.onPath) 
+            {
+                neighbours.Add(downRoom);
+            }
+        }
+        return neighbours;
+    }
+    void PlaceRooms() 
+    {
+        foreach (Room room in rooms) 
+        {
+            room.PlaceRoom();
+        }
+    }
+}
---
+++ Lab3/Assets/Bryant/Scripts/Room.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading;
+
+public class Room : MonoBehaviour
+{
+    public bool onPath, isEntrance, isExit;
+    public int gridX, gridY;
+
+    public List<Room> neighbours;
+    public List<string> openings = new List<string>();
+    public GameObject[] bases;
+
+
+
+    TMP_Text roomLabel;
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        roomLabel = GetComponentInChildren<TMP_Text>();
+    }
+    public void UpdateInfo() 
+    {
+        roomLabel.text = ("[" + gridX + "," + gridY + "]");
+        if (onPath) 
+        {
+            roomLabel.color = Color.yellow;
+        }
+        if (isEntrance) 
+        {
+            roomLabel.color = Color.green;
+        }
+        if (isExit) 
+        {
+            roomLabel.color = Color.red;
+        }
+    }
+    public GameObject PickBase() 
+    {
+        GameObject roomBase;
+        List<GameObject> possibleBases = new List<GameObject>();
+        for (int i = 0; i < bases.Length - 1; i++) 
+        {
+            if (onPath)
+            {
+                RoomBaseInfo rbInfo = bases[i].GetComponent<RoomBaseInfo>();
+                bool containsAllOpenings = false;
+                foreach (string k in openings) 
+                {
+                    if (rbInfo.roomType.Contains(k) == false)
+                    {
+                        containsAllOpenings = false;
+                        break;
+                    }
+                    else 
+                    {
+                        containsAllOpenings = true;
+                    }
+                }
+                if (containsAllOpenings) 
+                {
+                    possibleBases.Add(bases[i]);
+                }
+
+            }
+            else 
+            {
+                possibleBases.Add(bases[i]);
+            }
+        }
+        int roomBaseIndex = Random.Range(0, possibleBases.Count - 1);
+        roomBase = possibleBases[roomBaseIndex];
+        return roomBase;
+    }
+    public void PlaceRoom() 
+    {
+        GameObject roomBase = PickBase();
+        roomBase = (GameObject)Instantiate(roomBase);
+        roomBase.transform.parent = transform;
+        roomBase.transform.localPosition = Vector3.zero;
+    }
+}
